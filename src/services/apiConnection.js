9b69515a71dab0bbea1602a538d791f29c573ad0
Aryan_kamboj <aryankambozz@gmail.com>
2024-05-12 17:54:06 +0530

    cors fixed

--- src/services/apiConnection.js
+++ src/services/apiConnection.js
@@ -19,14 +19,12 @@ export const apiConnector = ({
   bodyData,
   params,
   headers,
-  withCredentials,
 }) => {
   return axiosInstance({
     method: method ? `${method}` : null,
     url: url ? `${url}` : null,
     data: bodyData ? bodyData : null,
     headers: headers ? headers : null,
-    withCredentials: withCredentials ? true : false,
     params: params ? params : null,
   })
 };
